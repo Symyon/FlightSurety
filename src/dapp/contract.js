45faf9ffa504d74d8e0266f56d10a94e6bcaab04
Simion Nacu <symyon.tech@gmail.com>
2022-09-26 16:31:02 -0400

    Changing wallet active account in MetaMask is reflcted in the dapp UI and functionality

--- src/dapp/contract.js
+++ src/dapp/contract.js
@@ -1,56 +1,151 @@
-import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
-import Config from './config.json';
-import Web3 from 'web3';
+import FlightSuretyApp from "../../build/contracts/FlightSuretyApp.json";
+import FlightSuretyData from "../../build/contracts/FlightSuretyData.json";
+import Config from "./config.json";
+import Web3 from "web3";
 
 export default class Contract {
-    constructor(network, callback) {
+  constructor(network, callback) {
+    let config = Config[network];
+    this.initialize(config, callback);
+    this.owner = null;
+    this.airlines = [];
+    this.passengers = [];
+    this.appContractAddress = config.appAddress;
+    this.dataContractAddress = config.dataAddress;
+    this.getActiveWalletAccount.bind(this);
+    this.setActiveWalletAccount.bind(this);
+  }
 
-        let config = Config[network];
-        this.web3 = new Web3(new Web3.providers.HttpProvider(config.url));
-        this.flightSuretyApp = new this.web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
-        this.initialize(callback);
-        this.owner = null;
-        this.airlines = [];
-        this.passengers = [];
+  getActiveWalletAccount() {
+    let self = this;
+    self.web3.eth.getAccounts((error, accts) => {
+      self.owner = accts[0];
+    });
+    return self.owner;
+  }
+
+  setActiveWalletAccount(acct) {
+    this.owner = acct;
+  }
+
+  async initialize(config, callback) {
+    if (window.ethereum) {
+      this.web3Provider = window.ethereum;
+      try {
+        // Request account access
+        await window.ethereum.enable();
+      } catch (error) {
+        // User denied account access...
+        console.error("User denied account access");
+      }
     }
-
-    initialize(callback) {
-        this.web3.eth.getAccounts((error, accts) => {
-           
-            this.owner = accts[0];
-
-            let counter = 1;
-            
-            while(this.airlines.length < 5) {
-                this.airlines.push(accts[counter++]);
-            }
-
-            while(this.passengers.length < 5) {
-                this.passengers.push(accts[counter++]);
-            }
-
-            callback();
-        });
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      this.web3Provider = window.web3.currentProvider;
     }
-
-    isOperational(callback) {
-       let self = this;
-       self.flightSuretyApp.methods
-            .isOperational()
-            .call({ from: self.owner}, callback);
+    // If no injected web3 instance is detected, fall back to Ganache
+    else {
+      this.web3Provider = new Web3.providers.HttpProvider(config.url);
     }
+    this.web3 = new Web3(this.web3Provider);
 
-    fetchFlightStatus(flight, callback) {
-        let self = this;
-        let payload = {
-            airline: self.airlines[0],
-            flight: flight,
-            timestamp: Math.floor(Date.now() / 1000)
-        } 
-        self.flightSuretyApp.methods
-            .fetchFlightStatus(payload.airline, payload.flight, payload.timestamp)
-            .send({ from: self.owner}, (error, result) => {
-                callback(error, payload);
-            });
-    }
+    this.getActiveWalletAccount();
+
+    this.flightSuretyApp = new this.web3.eth.Contract(
+      FlightSuretyApp.abi,
+      config.appAddress
+    );
+    this.flightSuretyData = new this.web3.eth.Contract(
+      FlightSuretyData.abi,
+      config.dataAddress
+    );
+
+    this.web3.eth.getAccounts((error, accts) => {
+      let counter = 1;
+
+      while (this.airlines.length < 5) {
+        this.airlines.push(accts[counter++]);
+      }
+
+      while (this.passengers.length < 5) {
+        this.passengers.push(accts[counter++]);
+      }
+
+      callback();
+    });
+  }
+
+  isOperational(callback) {
+    let self = this;
+    self.flightSuretyApp.methods
+      .isOperational()
+      .call({ from: self.owner }, callback);
+  }
+
+  fetchAirlineInfo(airline, callback) {
+    let self = this;
+    console.log("we are here contract info", airline);
+    console.log("owner is:", self.owner);
+    console.log("Testing contract", this.appContractAddress);
+
+    self.flightSuretyData.methods
+      .isAuthorized(this.appContractAddress)
+      .call({ from: self.owner }, (error, result) => {
+        if (error) {
+          console.log("isAuthorized error is:", error);
+        } else {
+          console.log("isAuthorized", result);
+        }
+      });
+
+    // this.flightSuretyData.methods
+    //   .setAuthorization(this.appContractAddress, true)
+    //   .send({ from: self.owner }, (error, result) => {
+    //     if (error) {
+    //       console.log("setAuthorization error is:", error);
+    //     } else {
+    //       console.log("setAuthorization", result);
+    //       self.flightSuretyData.methods
+    //         .isAuthorized(this.appContractAddress)
+    //         .call({ from: self.owner }, (error, result) => {
+    //           if (error) {
+    //             console.log("isAuthorized error is:", error);
+    //           } else {
+    //             console.log("isAuthorized", result);
+    //           }
+    //         });
+    //     }
+    //   });
+
+    // this.flightSuretyData.methods
+    //   .setAuthorization(self.appContractAddress, true)
+    //   .call({ from: self.owner }, (error, result) => {
+    //     if (error) {
+    //       console.log("setAuthorization error is:", error);
+    //     } else {
+    //       console.log("setAuthorization", result);
+
+    //     }
+    //   });
+    // self.flightSuretyApp.methods
+    //   .getAirlineInfo(airline)
+    //   .call({ from: self.owner }, (error, result) => {
+    //     console.log("fetchAirlineInfo", error, result);
+    //     // callback(error, payload);
+    //   });
+  }
+
+  fetchFlightStatus(flight, callback) {
+    let self = this;
+    let payload = {
+      airline: self.airlines[0],
+      flight: flight,
+      timestamp: Math.floor(Date.now() / 1000),
+    };
+    self.flightSuretyApp.methods
+      .fetchFlightStatus(payload.airline, payload.flight, payload.timestamp)
+      .send({ from: self.owner }, (error, result) => {
+        callback(error, payload);
+      });
+  }
 }
--- src/dapp/index.js
+++ src/dapp/index.js
@@ -1,55 +1,101 @@
+import DOM from "./dom";
+import Contract from "./contract";
+import "./flightsurety.css";
 
-import DOM from './dom';
-import Contract from './contract';
-import './flightsurety.css';
+(async () => {
+  let result = null;
 
+  let contract = new Contract("localhost", () => {
+    updateActiveAccountDisplayed(contract.getActiveWalletAccount());
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", function (accounts) {
+        const owner = contract.getActiveWalletAccount();
+        contract.setActiveWalletAccount(accounts[0]);
+        updateActiveAccountDisplayed(contract.getActiveWalletAccount());
+      });
+    }
 
-(async() => {
-
-    let result = null;
-
-    let contract = new Contract('localhost', () => {
-
-        // Read transaction
-        contract.isOperational((error, result) => {
-            console.log(error,result);
-            display('Operational Status', 'Check if contract is operational', [ { label: 'Operational Status', error: error, value: result} ]);
-        });
-    
-
-        // User-submitted transaction
-        DOM.elid('submit-oracle').addEventListener('click', () => {
-            let flight = DOM.elid('flight-number').value;
-            // Write transaction
-            contract.fetchFlightStatus(flight, (error, result) => {
-                display('Oracles', 'Trigger oracles', [ { label: 'Fetch Flight Status', error: error, value: result.flight + ' ' + result.timestamp} ]);
-            });
-        })
-    
+    // Read transaction
+    contract.isOperational((error, result) => {
+      // console.log(error, result);
+      display("Operational Status", "Check if contract is operational", [
+        { label: "Operational Status", error: error, value: result },
+      ]);
     });
-    
 
+    // User-submitted transaction
+    // DOM.elid("fetch-airline").addEventListener("click", () => {
+    //   let airline = DOM.elid("airline-address").value;
+    //   // Write transaction
+    //   console.log("wearehere");
+    //   contract.fetchAirlineInfo(airline, (error, result) => {
+    //     console.log("wearehere fetch");
+    //     displayAirlineInfo("Oracles", "Trigger oracles", [
+    //       {
+    //         label: "Fetch Airline Info",
+    //         error: error,
+    //         value: JSON.stringify(result),
+    //       },
+    //     ]);
+    //   });
+    // });
+
+    // User-submitted transaction
+    // DOM.elid("submit-oracle").addEventListener("click", () => {
+    //   let flight = DOM.elid("flight-number").value;
+    //   // Write transaction
+    //   contract.fetchFlightStatus(flight, (error, result) => {
+    //     display("Oracles", "Trigger oracles", [
+    //       {
+    //         label: "Fetch Flight Status",
+    //         error: error,
+    //         value: result.flight + " " + result.timestamp,
+    //       },
+    //     ]);
+    //   });
+    // });
+  });
 })();
 
-
-function display(title, description, results) {
-    let displayDiv = DOM.elid("display-wrapper");
-    let section = DOM.section();
-    section.appendChild(DOM.h2(title));
-    section.appendChild(DOM.h5(description));
-    results.map((result) => {
-        let row = section.appendChild(DOM.div({className:'row'}));
-        row.appendChild(DOM.div({className: 'col-sm-4 field'}, result.label));
-        row.appendChild(DOM.div({className: 'col-sm-8 field-value'}, result.error ? String(result.error) : String(result.value)));
-        section.appendChild(row);
-    })
-    displayDiv.append(section);
-
+function updateActiveAccountDisplayed(newAccount) {
+  const accountAddressElements = DOM.elementsWithId("#active-account");
+  accountAddressElements.forEach((node) => (node.textContent = newAccount));
 }
 
+function display(title, description, results) {
+  // let displayDiv = DOM.elid("display-wrapper");
+  // let section = DOM.section();
+  // section.appendChild(DOM.h2(title));
+  // section.appendChild(DOM.h5(description));
+  // results.map((result) => {
+  //   let row = section.appendChild(DOM.div({ className: "row" }));
+  //   row.appendChild(DOM.div({ className: "col-sm-4 field" }, result.label));
+  //   row.appendChild(
+  //     DOM.div(
+  //       { className: "col-sm-8 field-value" },
+  //       result.error ? String(result.error) : String(result.value)
+  //     )
+  //   );
+  //   section.appendChild(row);
+  // });
+  // displayDiv.append(section);
+}
 
-
-
-
-
-
+function displayAirlineInfo(title, description, results) {
+  // let displayDiv = DOM.elid("display-airline-wrapper");
+  // let section = DOM.section();
+  // section.appendChild(DOM.h2(title));
+  // section.appendChild(DOM.h5(description));
+  // results.map((result) => {
+  //   let row = section.appendChild(DOM.div({ className: "row" }));
+  //   row.appendChild(DOM.div({ className: "col-sm-4 field" }, result.label));
+  //   row.appendChild(
+  //     DOM.div(
+  //       { className: "col-sm-8 field-value" },
+  //       result.error ? String(result.error) : String(result.value)
+  //     )
+  //   );
+  //   section.appendChild(row);
+  // });
+  // displayDiv.append(section);
+}
